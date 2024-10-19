ecbe248963998f6e9051c67ba2cb76ee65a132f3
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-19 19:57:20 +0530

    Created a Server was successful

--- WebServer.js
+++ WebServer.js
@@ -1,1 +1,13 @@
 const http = require("http");
+const fileSystem = require("fs");
+
+const PORT = 3000;
+
+const homePage = fileSystem.readFileSync("./Templates/index.html");
+const server = http.createServer((request, response)=>
+  {
+    response.end(homePage);
+    console.log("A New Request is Received");
+  }
+)
+server.listen(PORT,()=>{console.log(`The Server has started in localhost:${PORT}`)});
