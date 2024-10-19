bbf3094e63ecd56c0521577bbb083edd02a84411
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-19 23:28:01 +0530

    Serving Static Files was successful

--- WebServer.js
+++ WebServer.js
@@ -3,21 +3,25 @@ const fileSystem = require("fs");
 
 const PORT = 3000;
 
-const homePage = fileSystem.readFileSync("./Templates/index.html");
+const htmlFile = fileSystem.readFileSync("./Templates/index.html","utf-8");
 const server = http.createServer((request, response)=>
 {
   const path = request.url;
   if(path === '/' || path.toLowerCase() === '/home')
   {
-    response.end("You are in Home Page");
+    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in Home Page"));
   }
   else if(path.toLowerCase() === '/about')
   {
-    response.end("You are in About Page");
+    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in About Page"));
   }
   else if(path.toLowerCase() === '/contact')
   {
-    response.end("You are in Contact page");
+    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in Contact Page"));
+  }
+  else 
+  {
+    response.end(htmlFile.replace("{{%CONTENT%}}", "Error 404 Page"));
   }
 }
 )
