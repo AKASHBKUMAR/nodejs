766ac3ef801fe8566d670ccdc6e2bf956a797923
Akash Kumar B <akashbkumar30@gmail.com>
2024-10-20 13:14:47 +0530

    Transformed JSON Data to HTML File

--- WebServer.js
+++ WebServer.js
@@ -1,21 +1,39 @@
 const http = require("http");
 const fileSystem = require("fs");
+const url = require("url");
 
 const PORT = 3000;
 
-const htmlFile = fileSystem.readFileSync("./Templates/index.html","utf-8");
-const products = fileSystem.readFileSync("./Data/data.json","utf-8");
+const indexHtml = fileSystem.readFileSync("./Templates/index.html","utf-8");
+
+const products = JSON.parse(fileSystem.readFileSync("./Data/data.json","utf-8"));
+
+const productListHtml = fileSystem.readFileSync("./Templates/Product-list.html","utf-8");
+
+const productListHtmlArray = products.map((product)=>{
+  let output = productListHtml.replace("{{%IMAGE%}}", product.productImage);
+  output = output.replace("{{%NAME%}}", product.name);
+  output = output.replace("{{%MODELNAME%}}",product.modelName);
+  output = output.replace("{{%MODELNO%}}", product.modelNumber);
+  output = output.replace("{{%SIZE%}}", product.size);
+  output = output.replace("{{%CAMERA%}}", product.camera);
+  output = output.replace("{{%PRICE%}}", product.price);
+  output = output.replace("{{%COLOR%}}", product.color);
+  return output;
+})
+
+
 
 const server = http.createServer((request, response)=>
 {
-  const path = request.url;
+  const {query,pathname:path} = url.parse(request.url,true);
   if(path === '/' || path.toLowerCase() === '/home')
   {
     response.writeHead(200, {
       "content-type": "text/html",
       "content-encoding": "utf-8",
     });
-    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in Home Page"));
+    response.end(indexHtml.replace("{{%CONTENT%}}", "You are in Home Page"));
   }
   else if(path.toLowerCase() === '/about')
   {
@@ -23,7 +41,7 @@ const server = http.createServer((request, response)=>
       "content-type": "text/html",
       "content-encoding": "utf-8",
     });
-    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in About Page"));
+    response.end(indexHtml.replace("{{%CONTENT%}}", "You are in About Page"));
   }
   else if(path.toLowerCase() === '/contact')
   {
@@ -31,24 +49,25 @@ const server = http.createServer((request, response)=>
       "content-type": "text/html",
       "content-encoding": "utf-8",
     });
-    response.end(htmlFile.replace("{{%CONTENT%}}", "You are in Contact Page"));
+    response.end(indexHtml.replace("{{%CONTENT%}}", "You are in Contact Page"));
   }
   else if(path.toLowerCase() === "/product")
   {
-    response.writeHead(200, {
-      "content-type": "javascript-object",
-      "content-encoding": "utf-8",
+    
+    response.writeHead(200, 
+    {
+      "Content-Type": "text/html"
     });
-    response.end("You are in products page");
-    console.log(JSON.parse(products));
+    response.end(indexHtml.replace("{{%CONTENT%}}",productListHtmlArray.join(",")));
+    
   }
   else 
   {
     response.writeHead(404, {
       "Content-type": "text/html",
-      message: "Resource Not Found",
+      "message": "Resource Not Found",
     });
-    response.end(htmlFile.replace("{{%CONTENT%}}", "Error 404 Page"));
+    response.end(indexHtml.replace("{{%CONTENT%}}", "Error 404 Page"));
   }
 }
 )
